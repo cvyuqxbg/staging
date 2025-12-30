2464d46a8b971921597ea55abbed65b217f9e054
Mio <mio@mio19.uk>
2025-12-30 17:49:45 +1100

    kdePackages.kdeconnect-kde

--- flake.nix
+++ flake.nix
@@ -69,6 +69,7 @@
                       pipewire
                       openssh
                       nix
+                      kdePackages.kdeconnect-kde
                     ];
                   }
                 );
